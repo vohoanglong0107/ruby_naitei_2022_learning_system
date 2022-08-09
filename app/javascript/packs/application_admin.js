139dc622fbfdede1de21e12a1bf431b5d069ffb7
vohoanglong0107 <mamlong34@gmail.com>
2022-08-09 16:33:40 +0000

    52193-Setup_bootstrap-Header-Footer

--- app/javascript/packs/application_admin.js
+++ app/javascript/packs/application_admin.js
@@ -1,8 +1,9 @@
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+window.bootstrap = require("bootstrap");
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
