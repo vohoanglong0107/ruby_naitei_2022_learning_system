cdf898bde7778f02d2c73cbb9233e337abb8d1af
vohoanglong0107 <mamlong34@gmail.com>
2022-08-09 16:33:40 +0000

    52138-Init_layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  get "/signup", to: "users#new"
-  resources :users, only: %i(new create)
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    get "/help", to: "static_pages#help"
+    get "/about", to: "static_pages#about"
+    get "/signup", to: "users#new"
+
+    resources :users, only: %i(new create)
+    namespace :admin do
+      root to: "static_pages#index"
+      resources :static_pages
+    end
+  end
 end
